c0764b07a6318ef5d6dfa5e4a19bf169dee97ec5
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-06 10:51:59 +0200

    Add route constraint on catch-all route
    Let built-in PwaController handle /manifest.json

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
     end
   end
   root "homepage#index"
-  get "/*path" => "homepage#index"
+  get "/*path", to: "homepage#index", constraints: lambda { |request| request.original_fullpath != "/manifest.json" }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
